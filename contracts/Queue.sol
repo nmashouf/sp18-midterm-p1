9e5ff730e2569f0db8a618e70c97ff3b687a966a
Roham Ghotbi <ro.ghotbi@yahoo.com>
2018-03-08 11:13:33 -0800

    added Queue

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -10,50 +10,85 @@ pragma solidity ^0.4.15;
 contract Queue {
 	/* State variables */
 	uint8 size = 5;
+	unint time;
+	uint8 numOccupied;
 	// YOUR CODE HERE
+	address[] memory queue;
 
 	/* Add events */
 	// YOUR CODE HERE
 
 	/* Add constructor */
 	// YOUR CODE HERE
+	function Queue() {
+		queue = new address[](size);
+		time = now;
+		numOccupied = 0;
+	}
 
 	/* Returns the number of people waiting in line */
 	function qsize() constant returns(uint8) {
-		// YOUR CODE HERE
+		return numOccupied;
 	}
 
 	/* Returns whether the queue is empty or not */
 	function empty() constant returns(bool) {
 		// YOUR CODE HERE
+		return numOccupied == 0;
 	}
 	
 	/* Returns the address of the person in the front of the queue */
 	function getFirst() constant returns(address) {
 		// YOUR CODE HERE
+		return queue[0];
 	}
 	
 	/* Allows `msg.sender` to check their position in the queue */
 	function checkPlace() constant returns(uint8) {
 		// YOUR CODE HERE
+		for (uint8 i = 0; i<queue.length; i++){
+			if(msg.sender == queue[i]) {
+				return i;
+			}
+		}
+		return size + 1;	/* indicatetion of the fact that it is not in the list */
+
 	}
 	
 	/* Allows anyone to expel the first person in line if their time
 	 * limit is up
 	 */
 	function checkTime() {
-		// YOUR CODE HERE
+		/* our limit to be in front is an hour */
+		if (now - time > 60 * minutes || now - time < -60):
+			dequeue();
 	}
 	
 	/* Removes the first person in line; either when their time is up or when
 	 * they are done with their purchase
 	 */
 	function dequeue() {
-		// YOUR CODE HERE
+		/* removing the first person by moving everyone else one to the front*/
+		for (uint i = 0; i<queue.length; i++){
+			if(i != queue.length - 1) {
+				queue[i] = queue[i + 1];
+			} else {
+				queue[i] = 0;
+			}
+		}
+		numOccupied -= 1;
 	}
 
 	/* Places `addr` in the first empty position in the queue */
 	function enqueue(address addr) {
-		// YOUR CODE HERE
+		var msg = "The queue is Full, Please try again later!";
+		event listFull(msg);
+		if(numOccupied != queue.length) {
+			queue[numOccupied] = addr;
+				numOccupied += 1;
+		} else {
+			/*Not sure if printing like this works at all*/
+			listFull(msg);
+		}
 	}
 }
