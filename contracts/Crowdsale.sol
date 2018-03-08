04ffe47d02208c1491068e5c0558cfda08964ce0
Neeka Mashouf <neekamashouf@Neekas-MacBook-Pro.local>
2018-03-08 02:04:46 -0800

    updated a few functions

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -9,6 +9,79 @@ import './Token.sol';
  * Is timelocked, manages buyer queue, updates balances on `Token.sol`
  */
 
+
+
 contract Crowdsale {
 	// YOUR CODE HERE
+	//keep track of number tokens sold, start & end time, 
+	uint endTime;
+	uint initialNumTokens;
+	uint tokensPerWei;
+	uint tokensSold;
+	address public owner;
+	bool crowdsaleClosed = false;
+
+
+
+	mapping(address => uint) balanceOf;
+	Token public token;
+	Queue public queue;
+
+	function Crowdsale(uint _endtime, uint _initialNumTokens, uint _tokensPerWei, ) {
+		owner = msg.sender;
+		endtime = _endtime;
+		initialNumTokens = _initialNumTokens;
+		tokensPerWei = _tokensPerWei;
+
+	}
+
+	modifier requireOwner() {
+		if (msg.sender != owner) {
+			return;
+		}
+		_;
+	}
+
+	modifier withinTime() {
+		if (now < startTime || now > endTime){
+			return;
+		}
+		_;
+	}
+
+	modifier abletoBurn() {
+		require(totalSupply() > msg.value);
+		_;
+	}
+
+	// Owner only
+
+	//add new tokens to totalSupply
+	function mint(uint _numNewTokens) requireOwner() {
+		token.addTokens(_numNewTokens);
+	}
+
+	//remove tokens from totalSupply
+	function burn(uint _numTokens) requireOwner() abletoBurn() {
+		token.burnTokens(_numTokens);
+	}
+
+	function withdrawWhenClosed() requireOwner(){
+		if crowdsaleClosed{
+			transfer
+		}
+	}
+
+	//Buyers
+
+	function buyTokens(uint _numTokens) {
+		if (msg.sender == queue.getFirst() && queue.qsize() > 1) {
+			tokenSold += amount;
+
+		}
+
+	}
+
+
+
 }
